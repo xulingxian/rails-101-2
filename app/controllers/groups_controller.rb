b0c156e3cff12fe212988354f4a0a7f8b4dfebf3
xulingxian <xulingxian001@gmail.com>
2016-12-25 22:23:20 +0800

    implement is_member_of?(group)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,9 +7,9 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
-  def show
-    @group = Group.find(params[:id])
-    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
+ def show
+  @group = Group.find(params[:id])
+  @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
   def edit
   end
